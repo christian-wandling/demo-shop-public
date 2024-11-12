ffbc751643c2daa4c92b3917413f33440818af76
Christian Wandling <christian.wandling@gmail.com>
2024-11-12 13:18:12 +0700

    chore: add sonarcloud

--- jest.preset.js
+++ jest.preset.js
@@ -3,4 +3,5 @@ const nxPreset = require('@nx/jest/preset').default;
 module.exports = {
   ...nxPreset,
   setupFiles: ['jest-canvas-mock'],
+  coverageReporters: ['lcov', 'text', 'text-summary'],
 };
