ef38d6c8a994e01795eb4462d8fc6b3355725e2b
Christian Wandling <christian.wandling@gmail.com>
2024-12-30 22:08:52 +0700

    fixup! ci: add github runner

--- .ops/terraform/modules/api/ec2.tf
+++ .ops/terraform/modules/api/ec2.tf
@@ -48,7 +48,7 @@ resource "terraform_data" "api_deploy" {
     keycloak_realm            = var.keycloak_realm
     keycloak_url              = var.keycloak_address
     script_hash = filesha256("${path.module}/scripts/deploy.sh.tftpl"),
-    image_hash = filesha256(var.api_docker_image_path),
+    image_hash = var.api_docker_image_path && filesha256(var.api_docker_image_path),
   }
 
   connection {
