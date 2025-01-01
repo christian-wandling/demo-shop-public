9a6bf54f70c990559fa6b509614f6d6225762647
Christian Wandling <christian.wandling@gmail.com>
2025-01-01 23:56:51 +0700

    fixup! ci: add github runner

--- .ops/terraform/modules/frontend/locals.tf
+++ .ops/terraform/modules/frontend/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  frontend_domain = "${cloudflare_record.frontend.name}.${var.cloudflare_domain_name}"
+  frontend_domain  = "${cloudflare_record.frontend.name}.${var.cloudflare_domain_name}"
   cert_destination = "/home/ec2-user/certs"
-  key_destination = "/home/ec2-user/keys"
-  container_name = "demo-shop-frontend:latest"
+  key_destination  = "/home/ec2-user/keys"
+  container_name   = "demo-shop-frontend:latest"
 }
