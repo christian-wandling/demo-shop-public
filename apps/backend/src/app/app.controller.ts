355f2fb7b5e3a04b1162a43e2dbc7c6dbcb50ecb
Christian Wandling <christian.wandling@gmail.com>
2025-01-05 14:39:56 +0700

    fixup! ci: add dedicated workflows for version build and deploy

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,9 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller('app')
-export class AppController {
-  @Get('/test-sentry')
-  testSentry() {
-    throw new Error('Sentry test error from NestJS endpoint'); // This will be caught by the filter
-  }
-}
+export class AppController {}
