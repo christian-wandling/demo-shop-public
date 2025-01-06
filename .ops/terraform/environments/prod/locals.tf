f5cfc00fd89c0ca55b7380334b6205fa2322e0fc
Christian Wandling <christian.wandling@gmail.com>
2025-01-06 13:25:58 +0700

    chore: remove k8s

--- .ops/terraform/environments/prod/locals.tf
+++ .ops/terraform/environments/prod/locals.tf
@@ -1,6 +1,6 @@
 locals {
   cloudflare_ips = split("\n", trimspace(data.http.cloudflare_ips.response_body))
-  logger         = file("../../../scripts/utils/logger.sh")
+  logger         = file("../../../util/logger.sh")
   user           = "ec2-user"
   cdn_address    = "cdn.${var.cloudflare_domain_name}"
 }
